fde1d3a86665ebc62d3d4ab6320a006965a7fc11
katweenah <katrinapiado@gmail.com>
2023-11-30 15:49:00 -0800

    changed pickle

--- pages/4_Prediction.py
+++ pages/4_Prediction.py
@@ -31,10 +31,12 @@ df2 = pd.read_csv("pages/11_30_23_Pred_Data_Final1.csv")
 df2.drop(['WAITING_TIMERANGE'], axis=1, inplace=True)
 
 # Loading model and mapping pickle files
-rf_pickle = open("pages/11_30_23_rf_model_final.sav", 'rb') 
-rf_model = pd.read_pickle(r"pages/11_30_23_rf_model_final.sav")
+#rf_pickle = open("pages/11_30_23_rf_model_final.sav", 'rb') 
+#rf_model = pd.read_pickle(r"pages/11_30_23_rf_model_final.sav")
+with open("pages/11_30_23_rf_model_final.sav", 'rb') as f:
+    rf_model = pickle.load(f)
 #rf_model = pickle.load(rf_pickle) 
-rf_pickle.close() 
+    rf_pickle.close() 
 # Asking users to input their own data
 # penguin_file = st.file_uploader('Upload your own penguin data to train the model') 
 
