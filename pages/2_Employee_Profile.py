6dd7d9801cf1d0c0f6f8897f243f733b3c17b736
Katrina Apiado <78036270+kapiado@users.noreply.github.com>
2023-11-30 13:28:23 -0800

    Update 2_Employee_Profile.py

--- pages/2_Employee_Profile.py
+++ pages/2_Employee_Profile.py
@@ -16,7 +16,7 @@ st.markdown("# Employee Profile")
 c = st.empty()
 c.write("The Employee Profile includes key information about the demographic of employees in our dataset, who undergo the green card application process in the United States. This includes their education, salary, nationality, profession, major and layoff history. In the profile, we present visual representations of the data related to Immigration Backlog. We will explore the trend of immigration backlog overtime, in terms of applications received and waiting time for approval, for example. By examining these factors, we aim to gain insights into the patterns and changes within the immigration backlog, enabling a better understanding.")
 
-path = "C:/Users/kapia/multipage_app/HTML Files/"
+path = "HTML Files\"
 
 def NAICSvsNumCases():
     #HtmlFile = open(path+"NAICSvsNumCases.html", 'r', encoding='utf-8') v1
