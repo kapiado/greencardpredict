5fb199c7c4d8be842d94cc1f25e8f0665968da0e
katweenah <katrinapiado@gmail.com>
2023-12-01 11:49:55 -0800

    added showinfo df

--- pages/4_Prediction.py
+++ pages/4_Prediction.py
@@ -199,13 +199,13 @@ with st.form(key='my_form'):
 
     submit = st.form_submit_button('Submit', args=(1,
                     [codeInfo, wagelevelInfo, wageamountInfo, stateInfo, countryInfo, employeenumInfo,  admiclassInfo,  jobeducationInfo, expInfo, expmonthsInfo, layoffInfo, educationInfo]))
-# columns
-# 'NAICS_CODE', 'PW_LEVEL', 'PW_AMOUNT', 'WORK_STATE',
-#        'COUNTRY_OF_CITIZENSHIP', 'EMPLOYER_NUM_EMPLOYEES',
-#        'CLASS_OF_ADMISSION', 'JOB_EDUCATION', 'EXPERIENCE',
-#        'EXPERIENCE_MONTHS', 'LAYOFF_IN_PAST_SIX_MONTHS', 'WORKER_EDUCATION',
-#        'WAITING_TIMERANGE'
-
+# 
+showinfo = pd.DataFrame(data = [codeInfo, wagelevelInfo, wageamountInfo, stateInfo, countryInfo, employeenumInfo,  admiclassInfo,  jobeducationInfo, expInfo, expmonthsInfo, layoffInfo, educationInfo],columns=
+['NAICS_CODE', 'PW_LEVEL', 'PW_AMOUNT', 'WORK_STATE',
+        'COUNTRY_OF_CITIZENSHIP', 'EMPLOYER_NUM_EMPLOYEES',
+       'CLASS_OF_ADMISSION', 'JOB_EDUCATION', 'EXPERIENCE',
+        'EXPERIENCE_MONTHS', 'LAYOFF_IN_PAST_SIX_MONTHS', 'WORKER_EDUCATION'])
+showinfo
 
 
 # NAICS_CODE,PW_LEVEL,PW_AMOUNT,WORK_STATE,COUNTRY_OF_CITIZENSHIP,EMPLOYER_NUM_EMPLOYEES,CLASS_OF_ADMISSION,JOB_EDUCATION,EXPERIENCE,EXPERIENCE_MONTHS,LAYOFF_IN_PAST_SIX_MONTHS,WORKER_EDUCATION,WAITING_TIMERANGE
