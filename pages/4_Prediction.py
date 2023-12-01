1f24d380d393993f9f5fe750b68828f6f8fd3759
katweenah <katrinapiado@gmail.com>
2023-12-01 13:23:56 -0800

    df4

--- pages/4_Prediction.py
+++ pages/4_Prediction.py
@@ -217,12 +217,13 @@ df3 = pd.get_dummies(df3, columns = cat_var)
 user_encoded_df = df3.tail(1)
 
 df4 = pd.DataFrame(columns = ['NAICS_CODE', 'PW_LEVEL', 'PW_AMOUNT', 'WORK_STATE',
-#         'COUNTRY_OF_CITIZENSHIP', 'EMPLOYER_NUM_EMPLOYEES',
-#        'CLASS_OF_ADMISSION', 'JOB_EDUCATION', 'EXPERIENCE',
-#         'EXPERIENCE_MONTHS', 'LAYOFF_IN_PAST_SIX_MONTHS', 'WORKER_EDUCATION'])
-#df4.loc[len(df4)]=[codeInfo, wagelevelInfo, wageamountInfo, stateInfo, countryInfo, employeenumInfo,  admiclassInfo,  jobeducationInfo, expInfo, expmonthsInfo, layoffInfo, educationInfo])
-df5 = pd.DataFrame(data = [codeInfo, wagelevelInfo, wageamountInfo, stateInfo, countryInfo, employeenumInfo,  admiclassInfo,  jobeducationInfo, expInfo, expmonthsInfo, layoffInfo, educationInfo])
-pd.concat([df4, df5])
+         'COUNTRY_OF_CITIZENSHIP', 'EMPLOYER_NUM_EMPLOYEES',
+        'CLASS_OF_ADMISSION', 'JOB_EDUCATION', 'EXPERIENCE',
+         'EXPERIENCE_MONTHS', 'LAYOFF_IN_PAST_SIX_MONTHS', 'WORKER_EDUCATION'])
+df4.loc[-1]=[codeInfo, wagelevelInfo, wageamountInfo, stateInfo, countryInfo, employeenumInfo,  admiclassInfo,  jobeducationInfo, expInfo, expmonthsInfo, layoffInfo, educationInfo])
+df4
+#df5 = pd.DataFrame(data = [codeInfo, wagelevelInfo, wageamountInfo, stateInfo, countryInfo, employeenumInfo,  admiclassInfo,  jobeducationInfo, expInfo, expmonthsInfo, layoffInfo, educationInfo])
+#pd.concat([df4, df5])
 st.subheader("Predicting Waiting Time")
 
 
