cf8e844a304d2289f10694887236bb962c8bf6e8
katweenah <katrinapiado@gmail.com>
2023-12-04 15:24:44 -0800

    code

--- pages/2_Employee_Profile.py
+++ pages/2_Employee_Profile.py
@@ -37,8 +37,9 @@ def NAICSvsNumCases():
     #HtmlFile = open(path+"NAICSvsNumCases.html", 'r', encoding='utf-8') v1
     HtmlFile = open(path+"top10OccvsCases.html", 'r', encoding='utf-8')
     source_code = HtmlFile.read() 
-    print(source_code)
-    components.html(source_code,height=500, width=1000)
+    # print(source_code)
+    st.markdown(source_code, unsafe_allow_html=True)
+    # components.html(source_code,height=500, width=1000)
  
 def WTvsNAICS():
     HtmlFile = open(path+"AvgWTvsOcc.html", 'r', encoding='utf-8')
