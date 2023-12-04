9be43aa73caee05ec65ca6c5e9ca8fe76a820415
katweenah <katrinapiado@gmail.com>
2023-12-04 15:44:30 -0800

    fix

--- pages/2_Employee_Profile.py
+++ pages/2_Employee_Profile.py
@@ -17,7 +17,25 @@ c = st.empty()
 c.write("The Employee Profile includes key information about the demographic of employees in our dataset, who undergo the green card application process in the United States. This includes their education, salary, nationality, profession, major and layoff history. In the profile, we present visual representations of the data related to Immigration Backlog. We will explore the trend of immigration backlog overtime, in terms of applications received and waiting time for approval, for example. By examining these factors, we aim to gain insights into the patterns and changes within the immigration backlog, enabling a better understanding.")
 
 path = "HTML Files/"
-html_content = """
+
+
+def WTvsYear():
+    #HtmlFile = open(path+"AverageWaitingTimebyYear.html", 'r', encoding='utf-8') v1
+    HtmlFile = open(path+"Avgwtperyr.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    print(source_code)
+    components.html(source_code,height=500, width=700)
+
+def CasesvsYear():
+    #HtmlFile = open(path+"AverageWaitingTimebyYear.html", 'r', encoding='utf-8') v1
+    HtmlFile = open(path+"Casesperyr.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    print(source_code)
+    components.html(source_code,height=500,width=700)
+
+def NAICSvsNumCases():
+    #HtmlFile = open(path+"NAICSvsNumCases.html", 'r', encoding='utf-8') v1
+    html_content = """
 <!DOCTYPE html>
 <html>
 <head>
@@ -58,23 +76,6 @@ html_content = """
 </body>
 </html>
 """
-
-def WTvsYear():
-    #HtmlFile = open(path+"AverageWaitingTimebyYear.html", 'r', encoding='utf-8') v1
-    HtmlFile = open(path+"Avgwtperyr.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    print(source_code)
-    components.html(source_code,height=500, width=700)
-
-def CasesvsYear():
-    #HtmlFile = open(path+"AverageWaitingTimebyYear.html", 'r', encoding='utf-8') v1
-    HtmlFile = open(path+"Casesperyr.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    print(source_code)
-    components.html(source_code,height=500,width=700)
-
-def NAICSvsNumCases():
-    #HtmlFile = open(path+"NAICSvsNumCases.html", 'r', encoding='utf-8') v1
     with open(path+'top10OccvsCases.html', 'r') as f:
         html_content = f.read()
         print(html_content)
