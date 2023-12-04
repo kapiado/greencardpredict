d7d1b36c4d63a9d14b6ad8681e8db9d2f09b8843
katweenah <katrinapiado@gmail.com>
2023-12-04 13:49:29 -0800

    fixed syntax

--- pages/2_Employee_Profile.py
+++ pages/2_Employee_Profile.py
@@ -86,13 +86,13 @@ def HighestEducation():
     
 # tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(["Year","Industry","Nationality","Salary","Unit of Pay","Highest Education"])
 tab1, tab2 = st.tabs(["Year","Industry"])
-with tab1:
-    CasesvsYear()
-    WTvsYear()
+    with tab1:
+        CasesvsYear()
+        WTvsYear()
 
-with tab2:
-     NAICSvsNumCases()
-     WTvsNAICS()
+    with tab2:
+        NAICSvsNumCases()
+        WTvsNAICS()
 
 # with tab3:
 #     nationality()
